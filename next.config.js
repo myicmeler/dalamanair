4612f51fd562db34deb316eddbe79ba3da403c14
myicmeler <thenri66@gmail.com>
2026-05-19 17:26:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
-
 module.exports = nextConfig
