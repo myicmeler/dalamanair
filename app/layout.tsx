a42a990fa4554b16b2cc6fffc93c84fd16aeae73
myicmeler <thenri66@gmail.com>
2026-07-26 00:16:41 +0200

    Add GoatCounter script for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,6 +19,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Script
+          data-goatcounter="https://dalaman.goatcounter.com/count"
+          src="//gc.zgo.at/count.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   )
